38af7f71c87a6638b4990d4b0842783d3628f63d
Saif Ali <117920183+saifali845@users.noreply.github.com>
2026-06-02 15:56:49 +0530

    Update main.tf

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,5 +1,5 @@
 module "vm" {
-  source = "./modules/aks"
+  source = "../modules/aks"
 
 
   resource_group_name = var.resource_group
@@ -22,7 +22,7 @@ module "vm" {
 }
 
 module "aks" {
-  source = "modules/aks"
+  source = "../modules/aks"
 
 
   resource_group_name = var.resource_group
